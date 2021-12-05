0eea3017ec3313658a21f23a55b031afdbc37160
Cerem Cem ASLAN <cem@aktos.io>
2021-12-06 02:48:07 +0300

    permission handling changed. now there is no need to re-login

--- setup-permissions.sh
+++ setup-permissions.sh
@@ -1,21 +1,9 @@
 #!/bin/bash 
 
 brightness_file="/sys/class/backlight/intel_backlight/brightness"
-# You can use any group name you are already included.
-# see `id` output.
-group="${1:-video}"
- 
-echo "Using group name: $group"
-# Create the new group if it doesn't exist:
-if ! grep -q "^${group}:" /etc/group; then
-    echo "Adding new group: $group"
-    sudo addgroup $group
-    sudo usermod -a -G $group `whoami`
-    echo "Please logout and login to make the group change to take effect."
-fi
-
-sudo chgrp $group "$brightness_file"
-sudo chmod g+w "$brightness_file"
+set -x
+sudo chmod 666 $brightness_file
+set +x
 
 echo "Now you can directly write to the brightness file: "
 echo
