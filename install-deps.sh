7b341b2501302393036650267a94a68d69abdf43
Cerem Cem ASLAN <cem@aktos.io>
2024-09-19 19:28:47 +0300

    changes made for Debian 12 (Bookworm)

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# There is a workaround needed while installing PySide2:
-TMPDIR='/var/tmp' python3 -m pip install PySide2
-
-sudo apt install redshift
+sudo apt install python3-pyside2.qtwidgets python3-pyside2.qtuitools redshift
